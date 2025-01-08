279f00f84be4734d4d03957800b80226e7fcda2c
seonrizee <seonrizee@gmail.com>
2025-01-08 14:14:43 +0900

    스프링 핵심 원리 - 기본편: 싱글톤 컨테이너

--- course-spring-basic/core/src/test/java/hello/core/singleton/SingletonTest.java
+++ course-spring-basic/core/src/test/java/hello/core/singleton/SingletonTest.java
@@ -7,6 +7,8 @@ import hello.core.member.MemberService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class SingletonTest {
 
@@ -50,5 +52,22 @@ public class SingletonTest {
         // equal equals메소드
 
         singletonService1.logic();
+    }
+
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    void springContainer() {
+
+//        AppConfig appConfig = new AppConfig();
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+        // 참조값이 같은 것을 확인
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        // memberService 1 != memberService2
+        assertThat(memberService1).isSameAs(memberService2);
     }
 }
