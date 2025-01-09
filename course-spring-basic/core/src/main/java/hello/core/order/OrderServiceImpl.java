d44abaf206e540d2679d96095bd936b2efc27f67
seonrizee <seonrizee@gmail.com>
2025-01-09 22:30:30 +0900

    스프링 핵심 원리 - 기본편: 다양한 의존관계 주입 방법

--- course-spring-basic/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ course-spring-basic/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -9,14 +9,48 @@ import org.springframework.stereotype.Component;
 @Component
 public class OrderServiceImpl implements OrderService {
 
+    // 필드 주입
+//    @Autowired private MemberRepository memberRepository;
+//    @Autowired private DiscountPolicy discountPolicy;
+
+    // 수정자 주입
+
+//    private MemberRepository memberRepository;
+//    private DiscountPolicy discountPolicy;
+
+//    @Autowired
+//    public void setMemberRepository(MemberRepository memberRepository) {
+//        System.out.println("memberRepository = " + memberRepository);
+//        this.memberRepository = memberRepository;
+//    }
+//
+//    @Autowired
+//    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
+//        System.out.println("discountPolicy = " + discountPolicy);
+//        this.discountPolicy = discountPolicy;
+//    }
+
+    // 생성자 주입
+
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
+    // @Autowired 생성자가 1개면 생략 가능
     @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
+
+    // 일반 메서드 주입
+//    private MemberRepository memberRepository;
+//    private DiscountPolicy discountPolicy;
+//
+//    @Autowired
+//    public void init(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+//        this.memberRepository = memberRepository;
+//        this.discountPolicy = discountPolicy;
+//    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
